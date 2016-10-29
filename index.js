b870ecc7aff0cd4cc0b666e57b7ae43d634c3448
chrisonntag <sonntagc@fim.uni-passau.de>
2016-10-29 15:22:22 +0200

    Implement /add
    Adds a string to an array called pizzaArray
    when the user sends /add.

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+var TelegramBot = require('node-telegram-bot-api');
 var dotenv = require('dotenv');
+
 dotenv.load();
 
+/**
+* /add - adds a pizza
+* /getMeThePizza - prints a list of pizzas
+* /end - deletes the old list
+*/
+
 var token = process.env.TELEGRAM_TOKEN;
+
+var bot = new TelegramBot(token, {polling: true});
+
+pizzaArray = [];
+
+// Matches /start
+bot.onText(/\/add (.+)/, function (msg, match) {
+	var user = msg.from.id;
+ 	var resp = match[1];
+	pizzaArray.push(
+		{
+			id : msg.from.id,
+			first_name: msg.from.first_name,
+			last_name: msg.from.last_name,
+			username: msg.from.username,
+			pizza: resp
+		}
+	);
+	console.log(pizzaArray);
+});
